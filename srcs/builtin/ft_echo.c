c5a98ce6f575408313c9abb9b1c35d93ad0e619a
Mustafa Karakulak <mustafakarakulak@192.168.1.89>
2023-05-14 20:54:24 +0300

    echo is working

--- srcs/builtin/ft_echo.c
+++ srcs/builtin/ft_echo.c
@@ -6,7 +6,7 @@
 /*   By: mustafakarakulak <mustafakarakulak@stud    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/04 19:04:59 by mkarakul          #+#    #+#             */
-/*   Updated: 2023/05/14 18:44:54 by mustafakara      ###   ########.fr       */
+/*   Updated: 2023/05/14 20:54:00 by mustafakara      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ int	ft_echo_param_checker(char *str)
 		return (0);
 	while (str[i])
 	{
-		if (str[i] != 'n');
+		if (str[i] != 'n')
 			return (0);
 		i++;
 	}
@@ -36,7 +36,12 @@ void	ft_echo(t_env *data)
 	t_arg	*temp;
 
 	temp = data->t_arg->next;
-	if (ft_strcmp(temp->arg, "-n") || ft_strcmp(temp->arg, "-"))
+	if (!temp)
+	{
+		ft_putstr_fd("\n", 1);
+		return ;
+	}
+	if (ft_echo_param_checker(temp->arg))
 		temp = temp->next;
 	while (temp)
 	{
@@ -45,6 +50,6 @@ void	ft_echo(t_env *data)
 			ft_putstr_fd(" ", 1);
 		temp = temp->next;
 	}
-	if (ft_echo_param_checker(data->t_arg->next->arg) == 1)
+	if (ft_echo_param_checker(data->t_arg->next->arg) == 0)
 		ft_putstr_fd("\n", 1);
 }
